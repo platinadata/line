1dae9a0579d9f191901fc65529bfdc4296056b70
Masahiro Ishiwata <heights.of.oz.from@gmail.com>
2025-11-16 18:22:04 +0900

    firebaseからデータを取得するよう修正

--- lib/screens/talk_list.dart
+++ lib/screens/talk_list.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:line/models/user.dart';
 import 'package:line/widgets/talkuser_container.dart';
@@ -10,21 +11,37 @@ class TalkListScreen extends StatefulWidget {
 }
 
 class _TalkListScreenState extends State<TalkListScreen> {
+  User? _user;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUser();
+  }
+
+  Future<void> _loadUser() async {
+    final doc = await FirebaseFirestore.instance
+        .collection('users')
+        .doc('user2')
+        .get();
+
+    final data = doc.data() as Map<String, dynamic>;
+
+    setState(() {
+      _user = User(
+        id: data['id'],
+        name: data['name'],
+        profileImageUrl:
+            data['profileImageUrl'] ?? 'https://picsum.photos/id/237/100/100',
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('トーク 一覧')),
-      body: Column(
-        children: [
-          TalkUserContainer(
-            user: User(
-              id: 1,
-              name: 'masahiro',
-              profileImageUrl: 'https://picsum.photos/id/237/100/100',
-            ),
-          ),
-        ],
-      ),
+      body: Column(children: [TalkUserContainer(user: _user!)]),
     );
   }
 }
